94f4ea5745d1bd81f9b620b3952fcf821e52b1c5
hide1120 <54133489+hide1120@users.noreply.github.com>
2020-05-23 22:43:52 +0900

    postsController 作成まで

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,11 @@ module TweetApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 投稿一覧トップページ
+  get "posts/index" => "posts#index"
+
+  # トップページ
+  get "/" => "home#top"
 end
